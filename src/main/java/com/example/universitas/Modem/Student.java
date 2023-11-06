d7746775d8cff002b13cf554797840b08ec5e36a
CinthiaSerna <cinthia_serna23211@elpoli.edu.co>
2023-11-06 18:36:28 -0500

    se crea student con sus atributos

---
+++ src/main/java/com/example/universitas/Modem/Student.java
@@ -0,0 +1,55 @@
+package com.example.universitas.Modem;
+
+
+public class Student {
+
+    public String nombre;
+    public int id;
+    private String cedula;
+
+    public Student(int id, String cedula, String nombre){
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public Student(String cedula){
+        this.cedula = cedula;
+    }
+
+    public Student() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+
+    @Override
+    public String toString() {
+        return "el estudiante se llama: "+ this.nombre+ " su documento es: "+ this.cedula;
+
+    }
+
+}
